46f3a77e223481250ae08ea9c1acc1aac7bd44c5
atarax <atarax>
2018-02-03 00:50:04 +0100

    bugfix

--- infrastructure/cleanup_cluster.sh
+++ infrastructure/cleanup_cluster.sh
@@ -6,10 +6,12 @@ source config/cluster.env
 # exit on errors by default
 set -e
 
+# delete whole cluster
+echo "Deleting cluster..."
+kops delete cluster ${CLUSTER_DNS_NAME} --yes
+
 # delete s3-bucket
 echo "Deleting S3-Bucket..."
 aws s3 rb s3://${AWS_S3_BUCKET_NAME} --force
 
-# delete whole cluster
-echo "Deleting cluster..."
-kops delete cluster ${CLUSTER_DNS_NAME} --yes
+
